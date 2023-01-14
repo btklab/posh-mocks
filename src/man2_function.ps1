6ab6bb98e61d30d5b0fc6e7452d469e82a81b23b
btklab <btklabggl@gmail.com>
2023-01-15 07:00:57 +0900

    Fix: error in while block conditional expression

--- src/man2_function.ps1
+++ src/man2_function.ps1
@@ -162,7 +162,7 @@ function man2 {
             [int] $bufCol = $Column
         }
         [int] $dispRow = [math]::Ceiling( ($fileList.Count) / $bufCol)
-        while (($lineWidthMax -gt $bufWidth) -or ($bufCol -eq 1)){
+        while (($lineWidthMax -gt $bufWidth) -and ($bufCol -gt 0)){
             [int] $dispRow = [math]::Ceiling( ($fileList.Count) / $bufCol)
             $lineWidthMax = $fileList `
                 | flat $dispRow `
@@ -174,7 +174,10 @@ function man2 {
         [string[]] $dispAry = $fileList `
                 | flat $dispRow `
                 | tateyoko `
-                | keta -l
+                | keta -l `
+                | ForEach-Object {
+                    Write-Output $($_.Trim())
+                }
         return $dispAry
     }
     if (($FunctionName) -and (Test-Path $FunctionName -PathType Container)){
