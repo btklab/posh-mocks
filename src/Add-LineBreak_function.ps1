6e5214aeb24d44db8bdce2ffc2e80e4c96ff189b
btklab <btklabggl@gmail.com>
2023-09-13 23:39:39 +0900

    Rename: Add-CrLf, Add-CrLf-EndOfFile to Add-LineBreak, Add-LineBreakEndOfFile

--- src/Add-LineBreak_function.ps1
+++ src/Add-LineBreak_function.ps1
@@ -1,15 +1,15 @@
 <#
 .SYNOPSIS
-    Add-CrLf - Insert a newline when it finds "`r`n" in a strings
+    Add-LineBreak - Insert linebreak when it finds "`r`n" in a strings
 
 .EXAMPLE
-    'abc`r`ndef',"ghi jkl" | Add-CrLf
+    'abc`r`ndef',"ghi jkl" | Add-LineBreak
     abc
     def
     ghi jkl
 
 #>
-filter Add-CrLf {
+filter Add-LineBreak {
     [string] $readLine = [string] $_
     if( $readLine -match '`r?`n' ){
         $splitbuf = $readLine -Split '`r?`n'
