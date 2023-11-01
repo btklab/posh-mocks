d78c9ce7281623a37fd7c68b6ecf20b32671dc43
btklab <btklabggl@gmail.com>
2023-11-01 23:45:26 +0900

    Add: property name existence test

--- src/Transpose-Property_function.ps1
+++ src/Transpose-Property_function.ps1
@@ -37,13 +37,13 @@
         | Measure-Summary `
         | Format-Table -AutoSize
 
-    Property       Count        Sum    Mean       SD     Min    Qt25 Median    Qt75       Max
-    --------       -----        ---    ----       --     ---    ---- ------    ----       ---
-    number          1035    1848.00    1.79     1.24    1.00    1.00 1         2.00      7.00
-    orbital_period   992 1986894.26 2002.92 26014.73    0.09    5.45 39.98   526.62 730000.00
-    mass             513    1353.38    2.64     3.82    0.00    0.23 1.26      3.06     25.00
-    distance         808  213367.98  264.07   733.12    1.35   32.56 55.25   180.00   8500.00
-    year            1035 2079388.00 2009.07     3.97 1989.00 2007.00 2010   2012.00   2014.00
+    Property         Count    Mean       SD     Min    Qt25  Median    Qt75       Max Outlier
+    --------         -----    ----       --     ---    ----  ------    ----       --- -------
+    number         1035.00    1.79     1.24    1.00    1.00    1.00    2.00      7.00   93.00
+    orbital_period  992.00 2002.92 26014.73    0.09    5.45   39.98  526.62 730000.00  126.00
+    mass            513.00    2.64     3.82    0.00    0.23    1.26    3.06     25.00   52.00
+    distance        808.00  264.07   733.12    1.35   32.56   55.25  180.00   8500.00  106.00
+    year           1035.00 2009.07     3.97 1989.00 2007.00 2010.00 2012.00   2014.00   32.00
 
     # Transpose object
     Import-Csv -Path planets.csv `
@@ -51,18 +51,17 @@
         | Transpose-Property -Property "Property" `
         | Format-Table -AutoSize
 
-    Property  number orbital_period    mass  distance       year
-    --------  ------ --------------    ----  --------       ----
-    Count       1035            992     513       808       1035
-    Sum      1848.00     1986894.26 1353.38 213367.98 2079388.00
-    Mean        1.79        2002.92    2.64    264.07    2009.07
-    SD          1.24       26014.73    3.82    733.12       3.97
-    Min         1.00           0.09    0.00      1.35    1989.00
-    Qt25        1.00           5.45    0.23     32.56    2007.00
-    Median         1          39.98    1.26     55.25       2010
-    Qt75        2.00         526.62    3.06    180.00    2012.00
-    Max         7.00      730000.00   25.00   8500.00    2014.00
-    Outlier       93            126      52       106         32
+    Property  number orbital_period   mass distance    year
+    --------  ------ --------------   ---- --------    ----
+    Count    1035.00         992.00 513.00   808.00 1035.00
+    Mean        1.79        2002.92   2.64   264.07 2009.07
+    SD          1.24       26014.73   3.82   733.12    3.97
+    Min         1.00           0.09   0.00     1.35 1989.00
+    Qt25        1.00           5.45   0.23    32.56 2007.00
+    Median      1.00          39.98   1.26    55.25 2010.00
+    Qt75        2.00         526.62   3.06   180.00 2012.00
+    Max         7.00      730000.00  25.00  8500.00 2014.00
+    Outlier    93.00         126.00  52.00   106.00   32.00
 
 #>
 function Transpose-Property
@@ -77,8 +76,20 @@ function Transpose-Property
         [PSObject] $InputObject
     )
     # get all property names
+    [bool] $PropertyExists = $False
     [String[]] $AllPropertyNames = ($input[0].PSObject.Properties).Name `
-        | Where-Object { $_ -ne $Property }
+        | ForEach-Object {
+            if ($_ -ne $Property){
+                Write-Output $_
+            } else {
+                $PropertyExists = $True
+            }
+        }
+    # is property name exists?
+    if ( -not $PropertyExists ){
+        Write-Error "Property name: ""$Property"" is not exists." -ErrorAction Stop
+    }
+    # get Axis property names
     [String[]] $AxisPropertyNames = $input | Select-Object -ExpandProperty $Property
     Write-Debug "AllProp  : $($AllPropertyNames  -join ', ')"
     Write-Debug "AxisProp : $($AxisPropertyNames -join ', ')"
